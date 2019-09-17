82931ed1ef62c518200f65dd60e3c2a663c18739
Sam Simmons <dev@ranchcountryventures.com>
2019-09-16 23:19:29 -0600

    Created initial JavaFX window for GUI

--- src/main/java/com/rvsmarthub/App.java
+++ src/main/java/com/rvsmarthub/App.java
@@ -1,13 +1,46 @@
 package com.rvsmarthub;
 
 /**
- * Hello world!
- *
+ *  JavaFX Imports
  */
-public class App 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *  Primary Application Class
+ *  ------------------------------------------------------------------------
+ *  Initializes application preferences and displays the GUI
+ */
+@SuppressWarnings("restriction")
+public class App extends Application 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	Button testButton; // Used as a test case
+	
+	public static void main(String[] args) throws InterruptedException {
+
+		launch(args);
+	 }
+
+	@Override
+	public void start(Stage mainWindow) throws Exception {
+
+		// Set window title
+		mainWindow.setTitle("RV Smart Hub");
+		
+		// Test Button
+		testButton = new Button();
+		testButton.setText("Test Button");
+		
+		// Main Window Layout
+		StackPane layout = new StackPane();
+		layout.getChildren().add(testButton);
+		
+		// Main Window
+		Scene scene = new Scene(layout, 1024, 600);
+		mainWindow.setScene(scene);
+		mainWindow.show();
+	}
 }
