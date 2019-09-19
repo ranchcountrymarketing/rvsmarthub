90877d7637c5d2641a1cd3e2aa1bfdda14126393
Sam <sam.simmons@ranchcountryventures.com>
2019-09-19 16:42:21 -0600

    Added resources directory with App.fxml

--- src/main/java/com/rvsmarthub/App.java
+++ src/main/java/com/rvsmarthub/App.java
@@ -4,10 +4,9 @@ package com.rvsmarthub;
  *  JavaFX Imports
  */
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -19,18 +18,9 @@ public class App extends Application
 {
 	// String Constants
 	private static String APP_NAME								= "RV Smart Hub";
-	private static String SETTINGS_SCENE_TITLE			= "Settings";
-	private static String EXIT_BUTTON_TXT 					= "Exit";
 	
 	// Main Scene Properties
 	Scene mainScene;
-	Label mainSceneTitle;
-	Button settingsButton;
-	
-	// Settings Scene Properties
-	Scene settingsScene;
-	Label settingsSceneTitle;
-	Button settingsExitButton;
 	
 	/**
 	 * main()
@@ -50,33 +40,11 @@ public class App extends Application
 	@Override
 	public void start(Stage mainStage) throws Exception {
 		
-		// Settings Scene
-		// -------------------------------------------------------
-		settingsSceneTitle = new Label();
-		settingsSceneTitle.setText(SETTINGS_SCENE_TITLE);
-		
-		settingsExitButton = new Button();
-		settingsExitButton.setText(EXIT_BUTTON_TXT);
-		settingsExitButton.setOnAction(e -> showScene(mainStage, mainScene, APP_NAME));
-		
-		VBox settingsLayout = new VBox();
-		settingsLayout.getChildren().addAll(settingsSceneTitle, settingsExitButton);
-		
-		Scene settingsScene =  new Scene(settingsLayout, 1024, 600);			
+		Parent root = FXMLLoader.load(getClass().getResource("/App.fxml"));
 		
 		// Main Stage / Scene
-		// -------------------------------------------------------	
-		mainSceneTitle = new Label();
-		mainSceneTitle.setText(APP_NAME);			
-		
-		settingsButton = new Button();
-		settingsButton.setText(SETTINGS_SCENE_TITLE);
-		settingsButton.setOnAction( e -> showScene(mainStage, settingsScene, SETTINGS_SCENE_TITLE));
-		
-		VBox mainLayout = new VBox();
-		mainLayout.getChildren().addAll(mainSceneTitle, settingsButton);
-		
-		mainScene = new Scene(mainLayout, 1024, 600);		
+		// -------------------------------------------------------			
+		mainScene = new Scene(root, 1024, 600);		
 		
 		showScene(mainStage, mainScene, APP_NAME);
 		
